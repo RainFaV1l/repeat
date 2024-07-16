514f83ff2e640da3a4e0ad597a2b738b8ee82909
Azat <Nazmiev_Flesh_Royal@mail.ru>
2024-07-16 13:51:25 +0300

    15.07.2024

--- src/Pages/Catalog.jsx
+++ src/Pages/Catalog.jsx
@@ -1,10 +1,14 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { PageHeaderComponent } from "../Components/PageHeaderComponent"
 import styles from "./Catalog.module.css"
 import { LoadingComponent } from "../Components/LoadingComponent"
 import { Product } from "../Components/Product"
+import { Popup } from "../Components/Popup"
+import { AppContext } from "../context/AppContext"
 
 export function Catalog() {
+
+    const [cartModal, setCartModal, cart, setCart, popup, setPopup] = useContext(AppContext)
 
     const [products, setProducts] = useState([])
     const [filteredProducts, setFilteredProducts] = useState([])
@@ -13,6 +17,7 @@ export function Catalog() {
     const [sort, setSort] = useState('')
 
     useEffect(() => {
+
         if(loading) {
             fetch('https://dummyjson.com/products')
             .then(response => response.json())
@@ -20,7 +25,31 @@ export function Catalog() {
                 setProducts(json.products)
                 setLoading(false)
             })
+        } 
+        
+        else {
+
+            // Пример использования IntersectionObserver
+            let observer = new IntersectionObserver((entries, observer) => {
+                entries.forEach((entry) => {
+                    if(entry.isIntersecting) {
+                        setPopup(true)
+                        // прекращаем наблюдение
+                        observer.unobserve(entry.target)
+                    }
+                })
+            }, {
+                root: null,
+                rootMargin: '0px',
+                threshold: 1
+            })
+
+            const emptyProducts = document.querySelector('.empty-products')
+
+            observer.observe(emptyProducts)
+
         }
+
         let filteredProductsArray = products;
         if (search.length > 0) {
             filteredProductsArray = filteredProductsArray.filter(product =>
@@ -41,12 +70,18 @@ export function Catalog() {
             filteredProductsArray = products
         }
         setFilteredProducts(filteredProductsArray);
+        
+
     }, [loading, products, search, sort])
 
     return (
         <>
             {/* Подключение компонента загрузки */}
             <LoadingComponent loading={loading}/>
+            {/* popup в виде модалки */}
+            {
+                popup ? <Popup/> : ''
+            }
             <div className={styles.products}>
                 <PageHeaderComponent title="Каталог товаров" subtitle="Лучшие товары только у нас!"/>
                 <div className={styles.filter}>
@@ -66,6 +101,7 @@ export function Catalog() {
                         })
                     }
                 </div>
+                <div className="empty-products" style={{ height: 100 + 'px'}}></div>
             </div>
         </>
     )
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -9,10 +9,11 @@ export function Root() {
 
     const [cartModal, setCartModal] = useState(false)
     const [cart, setCart] = useState([])
+    const [popup, setPopup] = useState(false)
 
     return (
         <>
-            <AppContext.Provider value={[cartModal, setCartModal, cart, setCart]}>
+            <AppContext.Provider value={[cartModal, setCartModal, cart, setCart, popup, setPopup]}>
                 <div className="wrapper">
                     { cartModal ? <Cart/> : '' }
                     <HeaderComponent/>
